465d932f29da5b4509b6b841141dca698d05c091
scollier <scollier@redhat.com>
2014-05-10 08:42:25 -0400

    mariadb

--- contrib/rhel-dockerfiles/mariadb/config_mariadb.sh
+++ contrib/rhel-dockerfiles/mariadb/config_mariadb.sh
@@ -3,23 +3,21 @@
 __mysql_config() {
 # Hack to get MySQL up and running... I need to look into it more.
 echo "Running the mysql_config function."
-yum -y erase community-mysql community-mysql-server
 rm -rf /var/lib/mysql/ /etc/my.cnf
-yum -y install community-mysql community-mysql-server
 mysql_install_db
 chown -R mysql:mysql /var/lib/mysql
-/usr/bin/mysqld_safe & 
+/usr/bin/mysqld_safe &
 sleep 10
 }
 
 __start_mysql() {
 echo "Running the start_mysql function."
 mysqladmin -u root password mariadbPassword
-mysql -uroot -pmysqlPassword -e "CREATE DATABASE testdb"
-mysql -uroot -pmysqlPassword -e "GRANT ALL PRIVILEGES ON testdb.* TO 'testdb'@'localhost' IDENTIFIED BY 'mysqlPassword'; FLUSH PRIVILEGES;"
-mysql -uroot -pmysqlPassword -e "GRANT ALL PRIVILEGES ON *.* TO 'testdb'@'%' IDENTIFIED BY 'mysqlPassword' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql -uroot -pmysqlPassword -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'mysqlPassword' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql -uroot -pmysqlPassword -e "select user, host FROM mysql.user;"
+mysql -uroot -pmariadbPassword -e "CREATE DATABASE testdb"
+mysql -uroot -pmariadbPassword -e "GRANT ALL PRIVILEGES ON testdb.* TO 'testdb'@'localhost' IDENTIFIED BY 'mariadbPassword'; FLUSH PRIVILEGES;"
+mysql -uroot -pmariadbPassword -e "GRANT ALL PRIVILEGES ON *.* TO 'testdb'@'%' IDENTIFIED BY 'mariadbPassword' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -uroot -pmariadbPassword -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'mariadbPassword' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -uroot -pmariadbPassword -e "select user, host FROM mysql.user;"
 killall mysqld
 sleep 10
 }
